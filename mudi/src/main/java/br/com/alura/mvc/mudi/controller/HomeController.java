8301deee5301214a5f2c07f4b7361ba73f437d63
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-08-01 14:44:30 -0300

    Spring MVC: crie um web app com Thymeleaf e Bootstrap
    Percorrendo a lista na view

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,13 +1,29 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("Xiaomi Redmi Note 8");
+		pedido.setUrlImagem("https://m.media-amazon.com/images/I/41YvfoWY9KL._AC_.jpg");
+		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-Camera/dp/B07ZJZ7VW6/ref=sr_1_4?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3PA6CTAFJNBB5&keywords=xiaomi+redmi+note+8&qid=1659374595&sprefix=xiaomi+redmi+note+8%2Caps%2C223&sr=8-4&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
+		pedido.setDescricao("Uma descrição qualquer...");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		model.addAttribute("pedidos", pedidos);
+		
 		return "home";
 	}
 
