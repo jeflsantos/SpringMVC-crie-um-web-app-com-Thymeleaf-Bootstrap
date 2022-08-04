ac76fff2c8152b7eb8ae24e6af43b3e701136393
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-08-04 15:00:13 -0300

    Spring MVC: crie um web app com Thymeleaf e Bootstrap
    Usando JPA puro

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,7 +1,10 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,16 +15,16 @@ import br.com.alura.mvc.mudi.model.Pedido;
 @Controller
 public class HomeController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
 		
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi Note 8");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/41YvfoWY9KL._AC_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-Camera/dp/B07ZJZ7VW6/ref=sr_1_4?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3PA6CTAFJNBB5&keywords=xiaomi+redmi+note+8&qid=1659374595&sprefix=xiaomi+redmi+note+8%2Caps%2C223&sr=8-4&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
-		pedido.setDescricao("Uma descrição qualquer...");
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
+		
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
